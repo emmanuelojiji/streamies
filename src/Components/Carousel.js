3b2cbdbd9712f390e24524f740c59adffc7a7518
emmanuelojiji <emmanuelojiji@hotmail.com>
2022-12-04 15:51:43 +0000

    Carousel Added

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,17 +1,32 @@
 import "./Carousel.scss";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import tems from "../media/billboard-img.png";
 import black_pink from "../media/blackpink.jpeg";
+import jack from "../media/jack.jpg";
+import bad_bunny from "../media/bad-bunny.webp";
 
 const Carousel = () => {
-  const [currentImage, setCurrentImage] = useState(black_pink);
+  const images = [tems, bad_bunny, black_pink, jack];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (currentIndex === images.length - 1) {
+        setCurrentIndex(0);
+      } else {
+        setCurrentIndex(currentIndex + 1);
+      }
+    }, 2000);
+    return () => clearInterval(intervalId);
+  });
 
   return (
     <div className="carousel-container">
-      <img src={currentImage} className="image-bg" />
-      <img src={currentImage} className="image-fg" />
+      <img src={images[currentIndex]} className="image-bg" />
+      <img src={images[currentIndex]} className="image-fg" />
     </div>
   );
 };
