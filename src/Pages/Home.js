6798a64eb8ee93feb4e4e7f77a19b6f2ccb31dd5
emmanuelojiji <emmanuelojiji@hotmail.com>
2022-12-04 16:28:30 +0000

    git ignorecase sensitive issue

---
+++ src/Pages/Home.js
@@ -0,0 +1,27 @@
+import "./Home.scss";
+import spotify_logo from "../media/spotify-logo.png";
+import logo from "../media/logo.svg";
+import Carousel from "../Components/Carousel";
+
+const Home = () => {
+  return (
+    <main>
+      <img src={logo} className="logo" />
+
+      <div className="main-content">
+        <div className="main-content-left">
+          <h1>Host your own award show using your Spotify account.</h1>
+          <a>BEGIN SHOW</a>
+        </div>
+
+        <Carousel />
+      </div>
+
+      <footer>
+        <img src={spotify_logo} className="spotify-logo" />
+      </footer>
+    </main>
+  );
+};
+
+export default Home;
